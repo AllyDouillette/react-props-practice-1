2cae9f1af39df63c8fcda4f2040bfc343cd82bfb
Rebecca Noy <rebecca.noy@gmx.de>
2023-10-27 11:26:21 +0200

    done?

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,33 @@ import { reverse } from './helpers'
 function App() {
   const [name, setName] = useState('Nicolas')
 
-  return (
-    <div className="app">
+  function printGreeting () {
+    return (
       <h1 className="title">Welcome, {name}!</h1>
-      <p className="name-length">
-        Did you know that {name} is {name.length} characters long?!
-      </p>
+    )
+  }
+
+  function printNameLength () {
+    return ( 
+    <p className="name-length">
+      Did you know that {name} is {name.length} characters long?!
+    </p>
+    )
+  }
+
+  function printReverseName () {
+    return (
       <p className="name-reversed">
         Also, {name} backwards is {reverse(name)}
       </p>
+    )
+  }
+
+  return (
+    <div className="app">
+      {printGreeting()}
+      {printNameLength()}
+      {printReverseName()}
     </div>
   )
 }
